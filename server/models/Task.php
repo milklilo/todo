0a1767b7fec47b50e65f3636c0d471d3bf09b898
milklilo <sshinichiro827@gmail.com>
2025-06-16 22:00:42 -0300

    first commit

---
+++ server/models/Task.php
@@ -0,0 +1,18 @@
+<?php 
+class task
+{
+    public int $id
+    public string $descripition
+    public int $done
+
+
+        public function __construct($descripition, $done = 0, $id = null)
+        {
+            $this->$id = $id
+            $this->descripition = $descripition;
+            $this->done = $done
+
+        }
+
+
+}
